61773f335fe23ba72d876cdebb5cacee5007f5bd
joey <doode1524@gmail.com>
2021-03-27 19:39:13 -0400

    created new stream form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    )
+class StreamCreate extends React.Component {
+    renderInput({input, label}) { // destructured formProps
+        return(
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
+    }
+
+    render(){
+        return (
+            <form className="ui form">
+                <Field name="title" label="Enter Title" component={this.renderInput} />
+                <Field name="description" label="Enter Description" component={this.renderInput} />
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate)
