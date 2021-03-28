f71695b86b152836038e7c4531864a73ef6ea7a7
joey <doode1524@gmail.com>
2021-03-27 21:21:11 -0400

    error message new stream form validations

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,25 +2,59 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
-    renderInput({input, label}) { // destructured formProps
+    renderError({error, touched}) {
+        if (touched && error) {
+            return(
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    renderInput = ({input, label, meta}) => { // destructured formProps
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return(
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
                 <input {...input} />
+                {this.renderError(meta)}
             </div>
         )
     }
 
+    onSubmit(formValues){
+        
+    }
+
     render(){
         return (
-            <form className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" label="Enter Title" component={this.renderInput} />
                 <Field name="description" label="Enter Description" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
 }
 
+const validate = (formValues) => {
+    const errors = {}
+
+    if (!formValues.title) {
+        errors.title = "You must enter a Title"
+    }
+
+    if (!formValues.description) {
+        errors.description = "You must enter a Description"
+    }
+
+    return errors
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate)
